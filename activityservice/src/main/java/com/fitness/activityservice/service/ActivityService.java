361924f4c92ab40ff9ec04e18d8e903046e63274
Mr-XX23 <starbalami00@gmail.com>
2025-11-16 13:02:54 +0545

    Added ai services

--- activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
+++ activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -5,6 +5,8 @@ import com.fitness.activityservice.dto.ActivityResponse;
 import com.fitness.activityservice.models.Activity;
 import com.fitness.activityservice.repository.ActivityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +15,9 @@ public class ActivityService {
 
     private final ActivityRepository activityRepository;
     private final UserValidationService userValidationService;
+    private final KafkaTemplate<String, Activity> kafkaTemplate;
+    @Value("${kafka.topic.name}")
+    private String topicName;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
@@ -32,6 +37,12 @@ public class ActivityService {
                 .build();
 
         Activity savedActivity = activityRepository.save(activity);
+
+        try {
+            kafkaTemplate.send(topicName, savedActivity.getId(), savedActivity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return mapToResponse(savedActivity);
     }
 
