f4e8fd8bde7f7989b15a958a80788b0b9814c8b6
Mr-XX23 <starbalami00@gmail.com>
2025-11-16 13:08:54 +0545

    Added ai activity class and interservice communications

---
+++ aiservice/src/main/java/com/fitness/aiservice/models/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.aiservice.models;
+
+public enum ActivityType {
+    RUNNING,
+    CYCLING,
+    SWIMMING,
+    WALKING,
+    YOGA,
+    STRENGTH_TRAINING,
+    HIIT,
+    DANCE,
+    PILATES,
+    OTHER
+}
